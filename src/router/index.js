a1b709f3a5feb1f41cf1b6c98de1dfef80c7a226
bien882 <bienb1909882@student.ctu.edu.vn>
2022-11-03 14:46:07 +0700

    Tao trang them moi mot lien he

--- src/router/index.js
+++ src/router/index.js
@@ -17,6 +17,12 @@ import ContactBook from "@/views/ContactBook.vue";
         component: () => import("@/views/ContactEdit.vue"),
         props: true // Truyền các biến trong $route.params vào làm props
     },
+    {
+        path: '/contacts/add',
+        name: 'contact.add',
+        component: () => import('@/views/ContactAdd.vue'),
+        props: true, // Truyền các biến trong $route.params vào làm props
+    },
 ];
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
