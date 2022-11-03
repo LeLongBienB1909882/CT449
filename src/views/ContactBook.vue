ade27503b79a6ca0a1c3e48fafb25549bfe5d507
bien882 <bienb1909882@student.ctu.edu.vn>
2022-11-03 13:52:35 +0700

    Tao trang cap nhat lien he

--- src/views/ContactBook.vue
+++ src/views/ContactBook.vue
@@ -18,7 +18,7 @@
                 <button class="btn btn-sm btn-primary" @click="refreshList()">
                     <i class="fas fa-redo"></i> Làm mới
                 </button>
-                <button class="btn btn-sm btn-success" @click="goToAddContact">
+                <button class="btn btn-sm btn-success" @click="refreshList()">
                     <i class="fas fa-plus"></i> Thêm mới
                 </button>
                 <button
@@ -36,6 +36,16 @@
                     <i class="fas fa-address-card"></i>
                 </h4>
                 <ContactCard :contact="activeContact" />
+                <router-link
+                    :to="{
+                        name: 'contact.edit',
+                        params: { id: activeContact._id },
+                    }"
+                    >
+                    <span class="mt-2 badge badge-warning">
+                    <i class="fas fa-edit"></i> Hiệu chỉnh</span
+                >
+                </router-link>
             </div>
         </div>
     </div>
